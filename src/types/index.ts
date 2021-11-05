1768cef07b657d5032f808f34eaea638ff02ee4b
Djibril <issoufoudjib@gmail.com>
2021-11-05 15:27:43 +0300

    model paginate and tojson puglin

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,1 +1,4 @@
 export { ENodeEnv } from './enums/node-env.enum';
+
+// interfaces
+export { IPaginateOption } from './interfaces/utils.interface';
---
+++ src/types/interfaces/utils.interface.ts
@@ -0,0 +1,11 @@
+export interface IPaginateOption {
+    sortBy?: string;
+    populate?: IPaginatePopulate[];
+    limit?: number;
+    page?: number;
+}
+
+interface IPaginatePopulate {
+    path: string;
+    match?: any;
+}
